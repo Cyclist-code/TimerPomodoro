76dfb49f46fdaace38d6dbc464cdaa393b938f94
Cyclist-code <47049219+Cyclist-code@users.noreply.github.com>
2021-08-26 17:27:27 +0700

    Adding the Home tab interface

--- TimerPomodoro/Forms/MainForm.Designer.cs
+++ TimerPomodoro/Forms/MainForm.Designer.cs
@@ -33,10 +33,14 @@ namespace TimerPomodoro
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.SideMenuTabControl = new MaterialSkin.Controls.MaterialTabControl();
             this.HomeTabPage = new System.Windows.Forms.TabPage();
+            this.StopButton = new MaterialSkin.Controls.MaterialButton();
+            this.StartButton = new MaterialSkin.Controls.MaterialButton();
+            this.TimerLabel = new MaterialSkin.Controls.MaterialLabel();
             this.SettingsTabPage = new System.Windows.Forms.TabPage();
             this.AboutTabPage = new System.Windows.Forms.TabPage();
             this.SideMenuImageList = new System.Windows.Forms.ImageList(this.components);
             this.SideMenuTabControl.SuspendLayout();
+            this.HomeTabPage.SuspendLayout();
             this.SuspendLayout();
             // 
             // SideMenuTabControl
@@ -57,6 +61,10 @@ namespace TimerPomodoro
             // 
             // HomeTabPage
             // 
+            this.HomeTabPage.BackColor = System.Drawing.Color.White;
+            this.HomeTabPage.Controls.Add(this.StopButton);
+            this.HomeTabPage.Controls.Add(this.StartButton);
+            this.HomeTabPage.Controls.Add(this.TimerLabel);
             this.HomeTabPage.ImageKey = "home.png";
             this.HomeTabPage.Location = new System.Drawing.Point(4, 39);
             this.HomeTabPage.Name = "HomeTabPage";
@@ -64,10 +72,61 @@ namespace TimerPomodoro
             this.HomeTabPage.Size = new System.Drawing.Size(586, 340);
             this.HomeTabPage.TabIndex = 0;
             this.HomeTabPage.Text = "Home";
-            this.HomeTabPage.UseVisualStyleBackColor = true;
+            // 
+            // StopButton
+            // 
+            this.StopButton.AutoSize = false;
+            this.StopButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.StopButton.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.StopButton.Depth = 0;
+            this.StopButton.HighEmphasis = true;
+            this.StopButton.Icon = null;
+            this.StopButton.Location = new System.Drawing.Point(279, 234);
+            this.StopButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.StopButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(140, 40);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "Stop";
+            this.StopButton.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.StopButton.UseAccentColor = false;
+            this.StopButton.UseVisualStyleBackColor = true;
+            // 
+            // StartButton
+            // 
+            this.StartButton.AutoSize = false;
+            this.StartButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.StartButton.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.StartButton.Depth = 0;
+            this.StartButton.HighEmphasis = true;
+            this.StartButton.Icon = null;
+            this.StartButton.Location = new System.Drawing.Point(121, 234);
+            this.StartButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.StartButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(140, 40);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.StartButton.UseAccentColor = false;
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // TimerLabel
+            // 
+            this.TimerLabel.AutoSize = true;
+            this.TimerLabel.Depth = 0;
+            this.TimerLabel.Font = new System.Drawing.Font("Roboto Light", 96F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.TimerLabel.FontType = MaterialSkin.MaterialSkinManager.fontType.H1;
+            this.TimerLabel.Location = new System.Drawing.Point(152, 86);
+            this.TimerLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.TimerLabel.Name = "TimerLabel";
+            this.TimerLabel.Size = new System.Drawing.Size(233, 115);
+            this.TimerLabel.TabIndex = 0;
+            this.TimerLabel.Text = "25:00";
             // 
             // SettingsTabPage
             // 
+            this.SettingsTabPage.BackColor = System.Drawing.Color.White;
             this.SettingsTabPage.ImageKey = "settings.png";
             this.SettingsTabPage.Location = new System.Drawing.Point(4, 39);
             this.SettingsTabPage.Name = "SettingsTabPage";
@@ -75,17 +134,16 @@ namespace TimerPomodoro
             this.SettingsTabPage.Size = new System.Drawing.Size(586, 340);
             this.SettingsTabPage.TabIndex = 1;
             this.SettingsTabPage.Text = "Settings";
-            this.SettingsTabPage.UseVisualStyleBackColor = true;
             // 
             // AboutTabPage
             // 
+            this.AboutTabPage.BackColor = System.Drawing.Color.White;
             this.AboutTabPage.ImageKey = "info.png";
             this.AboutTabPage.Location = new System.Drawing.Point(4, 39);
             this.AboutTabPage.Name = "AboutTabPage";
             this.AboutTabPage.Size = new System.Drawing.Size(586, 340);
             this.AboutTabPage.TabIndex = 2;
             this.AboutTabPage.Text = "About";
-            this.AboutTabPage.UseVisualStyleBackColor = true;
             // 
             // SideMenuImageList
             // 
@@ -110,6 +168,8 @@ namespace TimerPomodoro
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Timer Pomodoro";
             this.SideMenuTabControl.ResumeLayout(false);
+            this.HomeTabPage.ResumeLayout(false);
+            this.HomeTabPage.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -121,6 +181,9 @@ namespace TimerPomodoro
         private System.Windows.Forms.TabPage SettingsTabPage;
         private System.Windows.Forms.ImageList SideMenuImageList;
         private System.Windows.Forms.TabPage AboutTabPage;
+        private MaterialSkin.Controls.MaterialLabel TimerLabel;
+        private MaterialSkin.Controls.MaterialButton StartButton;
+        private MaterialSkin.Controls.MaterialButton StopButton;
     }
 }
 
