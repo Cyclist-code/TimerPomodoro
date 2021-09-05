912211c6930d8eef7c750045721073680e060fa2
Cyclist-code <47049219+Cyclist-code@users.noreply.github.com>
2021-09-05 11:51:44 +0700

    Create AppFormSettings

---
+++ TimerPomodoro/Services/AppFormSettings.cs
@@ -0,0 +1,12 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TimerPomodoro.Services
+{
+    public static class AppFormSettings
+    {
+
+    }
+}
