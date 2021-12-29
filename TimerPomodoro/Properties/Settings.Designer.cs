abb058cd1e5b650e34f33de9f9f1117db6de79e3
Cyclist-code <47049219+Cyclist-code@users.noreply.github.com>
2021-12-29 15:48:16 +0700

    Adding a language selection parameter in the application

--- TimerPomodoro/Properties/Settings.Designer.cs
+++ TimerPomodoro/Properties/Settings.Designer.cs
@@ -46,5 +46,17 @@ namespace TimerPomodoro.Properties {
                 this["ColorScheme"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Language {
+            get {
+                return ((string)(this["Language"]));
+            }
+            set {
+                this["Language"] = value;
+            }
+        }
     }
 }
