a7dc13a4c060ecbd6265416a8b3e1daa28037e72
Gostev Konstantin <KOSTYA190900@yandex.ru>
2022-01-16 14:16:17 +0700

    Small changes in the code

--- TimerPomodoro/Properties/Settings.Designer.cs
+++ TimerPomodoro/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace TimerPomodoro.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -44,6 +44,18 @@ namespace TimerPomodoro.Properties {
             }
             set {
                 this["ColorScheme"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int WorkMinutes {
+            get {
+                return ((int)(this["WorkMinutes"]));
+            }
+            set {
+                this["WorkMinutes"] = value;
             }
         }
     }
